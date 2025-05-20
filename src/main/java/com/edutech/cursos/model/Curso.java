d47c5a5aabfc24a7e08d4f0663609273c08fa27f
kristel <kr.acevedo@duocuc.cl>
2025-05-19 19:46:37 -1200

    Renombrado paquete entity a model en microservicio Cursos

--- src/main/java/com/edutech/cursos/model/Curso.java
+++ src/main/java/com/edutech/cursos/model/Curso.java
@@ -1,12 +1,11 @@
 package com.edutech.cursos.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
+
 @Data
 public class Curso {
     @Id
